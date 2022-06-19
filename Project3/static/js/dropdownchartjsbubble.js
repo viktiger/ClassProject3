bcd56c59c4a80fd3b3278b07327584f4a894d9d3
PBx0N <Patsiri.boonprasop@gmail.com>
2022-06-19 17:10:21 +1000

    updatedbubble

--- Project3/static/js/dropdownchartjsbubble.js
+++ Project3/static/js/dropdownchartjsbubble.js
@@ -1,79 +1,10 @@
+
 let DataOfState;
 
-
-// Start Charts
-d3.json("/Used/Age/Income/Register/State", function(rawData) {
-  
-  const parsingData = (rawData) => {
-    const result = {};
-
-    rawData.forEach(({ states_name, average_register, average_income, average_age_used }) => {
-
-      if (result[states_name]) {
-        result[states_name].push({ x: average_income, y: average_register, r: average_age_used });
-      } else {
-
-        result[states_name] = [{ x: average_income, y: average_register, r: average_age_used }];
-      }
-    });
-    
-    return result;
-  };
-
-  DataOfState = parsingData(rawData);
-
-  year_choice = DataOfState.filter(x=>x.report_year==2016)
-
-  states_each_year = year_choice.map(x=>x.states_name)
-  average_income_each_year = year_choice.map(x=>x.average_income)
-  average_register_each_year = year_choice.map(x=>x.year_choice)
-  average_age_used_each_year = year_choice.map(x=>x.average_age_used)
-  each_report_years = year_choice.map(x=>x.report_year)
-
-  var data_a = {
-    labels: states_each_year,
-    datasets: [
-      {
-        label: "Australian Capital Territory",
-        data: [DataOfState["Australian Capital Territory"]],
-        backgroundColor: false
-      },
-      {
-        label: "New South Wales",
-        data: DataOfState["New South Wales"],
-        backgroundColor: false,
-      },
-      {
-        label: "Victoria",
-        data: DataOfState["Victoria"],
-        backgroundColor: false,
-      },
-      {
-        label: "Northern Territory",
-        data: DataOfState["Northern Territory"],
-        backgroundColor: false,
-      },
-      {
-        label: "Queensland",
-        data: DataOfState["Queensland"],
-        backgroundColor: false,
-      },
-      {
-        label: "Tasmania",
-        data: DataOfState["Tasmania"],
-        backgroundColor: false,
-      },
-      {
-        label: "South Australia",
-        data: DataOfState["South Australia"],
-        backgroundColor: false,
-      },
-    ],
-  };
-
-  var config_a = {
+const renderbubbleChart = (data) => {
+  const config_a = {
     type: "bubble",
-    data: data_a,
+    data: data,
       options: {scales: {
         x: {
           position: "bottom",
@@ -91,89 +22,210 @@ d3.json("/Used/Age/Income/Register/State", function(rawData) {
       },
     },
   };
-  bubbleChart = new Chart(document.getElementById("bubbleChart"), config1);
-});
+bubbleChart = new Chart(document.getElementById("bubbleChart"), config_a);
+};
 
-
-function plot_chart(year){
+// Get data
+d3.json("/Used/Age/Income/Register/State", function(rawData) {
+  console.log(rawData);
   
-  d3.json("/Used/Age/Income/Register/State", function(DataOfState) {
+  const parsingData = (rawData) => {
+    const result = {};
 
-    DataOfState = parsingData(data);
+    rawData.forEach(({ states_name, report_year, average_register, average_income, average_age_used }) => {
 
-    year_choice = DataOfState.filter(x=>x.report_year==parseInt(year))
-  
-    states_each_year = year_choice.map(x=>x.states_name)
-    average_income_each_year = year_choice.map(x=>x.average_income)
-    average_register_each_year = year_choice.map(x=>x.year_choice)
-    average_age_used_each_year = year_choice.map(x=>x.average_age_used)
-    each_report_years = year_choice.map(x=>x.report_year)
-  
-    var data_a = {
-      labels: states_each_year,
-      datasets: [
-        {label: "Australian Capital Territory",
-        data: [DataOfState["Australian Capital Territory"]],
-        backgroundColor: false
+      if (result[states_name]) {
+        result[states_name].push({ average_income, average_register, average_age_used });
+      } else {
+
+        result[states_name] = [{ average_income, average_register, average_age_used }];
+      }
+    });
+    
+    return result;
+  };
+
+  DataOfState = parsingData(rawData);
+  console.log(DataOfState);
+  console.log(Object.keys(DataOfState));
+
+  const report_year = rawData
+  .map((item) => item.report_year)
+  .filter((value, index, self) => self.indexOf(value) === index);
+  console.log(report_year);
+
+  var data = {
+      //labels: report_year,
+      datasets: [{
+        type: "bubble",
+        label: Object.keys(DataOfState)[0],
+        data: [DataOfState[`${Object.keys(DataOfState)[0]}`]],
+        backgroundColor: "rgba(255, 138, 41, 0.5)",
       },
       {
-        label: "New South Wales",
-        data: DataOfState["New South Wales"],
-        backgroundColor: false,
+        type: "bubble",
+        label: Object.keys(DataOfState)[1],
+        data: [DataOfState[`${Object.keys(DataOfState)[1]}`]],
+        backgroundColor: "rgba(40, 90, 112, 0.5)",
       },
       {
-        label: "Victoria",
-        data: DataOfState["Victoria"],
-        backgroundColor: false,
+        type: "bubble",
+        label: Object.keys(DataOfState)[2],
+        data: [DataOfState[`${Object.keys(DataOfState)[2]}`]],
+        backgroundColor: "rgba(255, 205, 86, 0.5)",
       },
       {
-        label: "Northern Territory",
-        data: DataOfState["Northern Territory"],
-        backgroundColor: false,
+        type: "bubble",
+        label: Object.keys(DataOfState)[3],
+        data: [DataOfState[`${Object.keys(DataOfState)[3]}`]],
+        backgroundColor: "rgba(75, 192, 192, 0.5)",
       },
       {
-        label: "Queensland",
-        data: DataOfState["Queensland"],
-        backgroundColor: false,
+        type: "bubble",
+        label: Object.keys(DataOfState)[4],
+        data: [DataOfState[`${Object.keys(DataOfState)[4]}`]],
+        backgroundColor: "rgba(54, 162, 235, 0.5)",
       },
       {
-        label: "Tasmania",
-        data: DataOfState["Tasmania"],
-        backgroundColor: false,
+        type: "bubble",
+        label: Object.keys(DataOfState)[5],
+        data: [DataOfState[`${Object.keys(DataOfState)[5]}`]],
+        backgroundColor: "rgba(154, 262, 235, 0.5)",
       },
       {
-        label: "South Australia",
-        data: DataOfState["South Australia"],
-        backgroundColor: false,
+        type: "bubble",
+        label: Object.keys(DataOfState)[6],
+        data: [DataOfState[`${Object.keys(DataOfState)[6]}`]],
+        backgroundColor: "rgba(201, 203, 207, 0.5)",
       },
     ],
-    };
-    var config_a = {
-      type: "bubble",
-      data: data_a,
-        options: { scales: {
-          x: {
-            position: "bottom",
-            title: {
-              display: true,
-              text: "Average Income ",
-            },
-          },
-          y: {
-            title: {
-              display: true,
-              text: "Average Register",
-            },
-          },
-        },
-      },
-    };
-    bubbleChart = new Chart(document.getElementById("bubbleChart"), config_a);
-  });
-  
-}
+  };
+  renderbubbleChart(data);
+});
 
-function updateState(state) {
-  bubbleChart.config.data.datasets[0].data = DataOfState[state.value];
-  bubbleChart.update();
-}
+// // Plot charts
+// function plotGraph (error, Statedata, statesJson) {
+//   DataOfState = parsingData(Statedata);
+//   // setup
+//   const data = {
+//       labels: ["2016", "2017", "2018", "2019", "2020", "2021"],
+//       datasets: [
+//         {label: "Australian Capital Territory",
+//         data: [DataOfState["Australian Capital Territory"]],
+//         backgroundColor: "rgba(255, 138, 41, 0.5)",
+//       },
+//       {
+//         label: "New South Wales",
+//         data: DataOfState["New South Wales"],
+//         backgroundColor: "rgba(40, 90, 112, 0.5)",
+//       },
+//       {
+//         label: "Victoria",
+//         data: DataOfState["Victoria"],
+//         backgroundColor: "rgba(255, 205, 86, 0.5)",
+//       },
+//       {
+//         label: "Northern Territory",
+//         data: DataOfState["Northern Territory"],
+//         backgroundColor: "rgba(75, 192, 192, 0.5)",
+//       },
+//       {
+//         label: "Queensland",
+//         data: DataOfState["Queensland"],
+//         backgroundColor: "rgba(54, 162, 235, 0.5)",
+//       },
+//       {
+//         label: "Tasmania",
+//         data: DataOfState["Tasmania"],
+//         backgroundColor: "rgba(154, 262, 235, 0.5)",
+//       },
+//       {
+//         label: "South Australia",
+//         data: DataOfState["South Australia"],
+//         backgroundColor: "rgba(201, 203, 207, 0.5)",
+//       },
+//     ],
+//   };
+//   renderbubbleChart(data);
+// }
+
+
+
+
+// function plot_chart(year){
+  
+//   d3.json("/Used/Age/Income/Register/State", function(DataOfState) {
+
+//     DataOfState = parsingData(data);
+
+//     var data_a = {
+//       //labels: ["2016", "2017", "2018", "2019", "2020", "2021"],
+//       datasets: [
+//         {label: "Australian Capital Territory",
+//         data: [DataOfState["Australian Capital Territory"]],
+//         backgroundColor: "rgba(255, 138, 41, 0.5)",
+//       },
+//       {
+//         label: "New South Wales",
+//         data: DataOfState["New South Wales"],
+//         backgroundColor: "rgba(40, 90, 112, 0.5)",
+//       },
+//       {
+//         label: "Victoria",
+//         data: DataOfState["Victoria"],
+//         backgroundColor: "rgba(255, 205, 86, 0.5)",
+//       },
+//       {
+//         label: "Northern Territory",
+//         data: DataOfState["Northern Territory"],
+//         backgroundColor: "rgba(75, 192, 192, 0.5)",
+//       },
+//       {
+//         label: "Queensland",
+//         data: DataOfState["Queensland"],
+//         backgroundColor: "rgba(54, 162, 235, 0.5)",
+//       },
+//       {
+//         label: "Tasmania",
+//         data: DataOfState["Tasmania"],
+//         backgroundColor: "rgba(154, 262, 235, 0.5)",
+//       },
+//       {
+//         label: "South Australia",
+//         data: DataOfState["South Australia"],
+//         backgroundColor: "rgba(201, 203, 207, 0.5)",
+//       },
+//     ],
+//     };
+//     var config_a = {
+//       type: "bubble",
+//       data: data_a,
+//         options: { scales: {
+//           x: {
+//             position: "bottom",
+//             title: {
+//               display: true,
+//               text: "Average Income ",
+//             },
+//           },
+//           y: {
+//             title: {
+//               display: true,
+//               text: "Average Register",
+//             },
+//           },
+//         },
+//       },
+//     };
+//     bubbleChart = new Chart(document.getElementById("bubbleChart"), config_a);
+//   });
+  
+// }
+
+// Config
+
+
+// function updateState(state) {
+//   bubbleChart.config.data.datasets[0].data = DataOfState[state.value];
+//   bubbleChart.update();
+// }
